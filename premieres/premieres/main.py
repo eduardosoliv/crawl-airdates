c3a447542e7735f2bd0ce6eb24d54836c912872c
Eduardo Oliveira <me@eduardooliveira.net>
2020-12-27 21:14:47 +0000

    Initial commit of premieres

---
+++ premieres/premieres/main.py
@@ -0,0 +1,44 @@
+import click
+import requests
+from bs4 import BeautifulSoup
+
+airdates_baseurl = 'https://www.airdates.tv/'
+
+def get_page(url: str) -> requests.Response:
+    page = requests.get(url)
+    if not page.ok:
+      raise Exception("Error, unable to fetch " + url + " status code " + str(page.status_code))
+    return page
+
+def get_series(content: bytes, html_class: str) -> list:
+  soup = BeautifulSoup(content, 'html.parser')
+  series = list()
+  for elem in soup.findAll('div', {'class': html_class}):
+    series.append(extract_serie_name(elem.text))
+  if series == list():
+    raise Exception('Error, unable to find series on page content for class ' + html_class)
+  return series
+
+def extract_serie_name(serie_name : str) -> str:
+  return (serie_name.rsplit(' ', 1)[0]).strip('\n')
+
+def printseries(type: str):
+  airdates = get_page(airdates_baseurl)
+  series = get_series(airdates.content, 'entry ' + type + '-premiere')
+  for serie in series:
+    print(serie)
+
+@click.group()
+def premieres():
+    pass
+
+@premieres.command()
+def series(**kwargs):
+  printseries('series')
+
+@premieres.command()
+def season(**kwargs):
+  printseries('season')
+
+if __name__ == '__main__':
+    premieres()
---
+++ premieres/tests/test_main.py
@@ -0,0 +1,34 @@
+import pytest
+from unittest.mock import patch 
+
+from premieres import main
+
+def test_get_page():
+  with patch('exportseries.main.requests.get') as mock_request:
+    url = 'http://google.com/'
+    mock_request.return_value.ok = True
+    mock_request.return_value.status_code = 200
+    main.get_page(url)
+    mock_request.assert_called_once_with(url)
+
+def test_get_page_raises_exception():
+  with patch('exportseries.main.requests.get') as mock_request:
+    with pytest.raises(Exception):
+      mock_request.return_value.ok = False
+      mock_request.return_value.status_code = 404
+      url = 'http://google.com/'
+      main.get_page(url)
+
+def test_get_series_empty():
+  with pytest.raises(Exception):
+    main.get_series('')
+
+def test_get_series_find_all_elements():
+  assert 1 == len(main.get_series('<html><head></head><body><div class="entry season-premiere">All Rise S02E01</div></body></html>', 'entry season-premiere'))
+  assert 1 == len(main.get_series('<html><head></head><body><div class="entry series-premiere">All Rise S01E01</div></body></html>', 'entry series-premiere'))
+
+  assert 2 == len(main.get_series('<html><head></head><body><div class="entry season-premiere">All Rise S02E01</div><div class="entry season-premiere">The Expanse S05E01</div><div class="entry series-premiere">Attack On Titan S01E01</div></body></html>', 'entry season-premiere'))
+  assert 1 == len(main.get_series('<html><head></head><body><div class="entry season-premiere">All Rise S02E01</div><div class="entry season-premiere">The Expanse S05E01</div><div class="entry series-premiere">Attack On Titan S01E01</div></body></html>', 'entry series-premiere'))
+
+def test_extract_serie_name():
+  assert 'Frankie Drake Mysteries' == main.extract_serie_name('Frankie Drake Mysteries S01E01\n')
